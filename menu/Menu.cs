ddc2addcec4bd053bb13ed3a45435c95495464a8
rkhapov <r.khapov@yandex.ru>
2020-04-23 23:28:35 +0500

    fix menu

--- menu/Menu.cs
+++ menu/Menu.cs
@@ -1,226 +1,16 @@
 ﻿using System;
 using System.Numerics;
+using objects;
+using objects.Environment;
+using objects.Monsters;
+using objects.Weapons;
+using simple3d;
 using simple3d.Builder;
 using simple3d.Drawing;
 using simple3d.Levels;
-using simple3d.MathUtils;
 
 namespace menu
 {
-    internal abstract class BaseStaticMapObject : IMapObject
-    {
-        protected BaseStaticMapObject(Vector2 position, Vector2 size, float directionAngle, Sprite sprite)
-        {
-            Position = position;
-            Sprite = sprite;
-            Size = size;
-            DirectionAngle = directionAngle;
-        }
-
-        public Vector2 Position { get; }
-        public Vector2 Size { get; }
-        public float DirectionAngle { get; }
-        public Sprite Sprite { get; }
-
-        public abstract void OnWorldUpdate(Scene scene, float elapsedMilliseconds);
-        public void OnLeftMeleeAttack(Scene scene, MeleeWeapon weapon)
-        {
-            
-        }
-
-        public void OnRightMeleeAttack(Scene scene, MeleeWeapon weapon)
-        {
-            
-        }
-
-        public void OnShoot(Scene scene, ShootingWeapon weapon)
-        {
-            
-        }
-    }
-
-    internal abstract class BaseAnimatedStaticMapObject : IMapObject
-    {
-        private readonly Animation animation;
-    
-        protected BaseAnimatedStaticMapObject(Vector2 position, Vector2 size, float directionAngle, Animation animation)
-        {
-            Position = position;
-            Size = size;
-            DirectionAngle = directionAngle;
-            this.animation = animation;
-        }
-    
-        public Vector2 Position { get; }
-        public Vector2 Size { get; }
-        public float DirectionAngle { get; }
-        public Sprite Sprite => animation.CurrentFrame;
-    
-        public virtual void OnWorldUpdate(Scene scene, float elapsedMilliseconds)
-        {
-            animation.UpdateFrame(elapsedMilliseconds);
-        }
-
-        public void OnLeftMeleeAttack(Scene scene, MeleeWeapon weapon)
-        {
-            
-        }
-
-        public void OnRightMeleeAttack(Scene scene, MeleeWeapon weapon)
-        {
-            
-        }
-
-        public abstract void OnShoot(Scene scene, ShootingWeapon weapon);
-    }
-
-    internal class Ghost : BaseAnimatedStaticMapObject
-    {
-        public Ghost(Vector2 position, Vector2 size, float directionAngle, Animation animation) : base(position, size, directionAngle, animation)
-        {
-        }
-
-        public override void OnShoot(Scene scene, ShootingWeapon weapon)
-        {
-            Console.WriteLine($"{this} has been hit");
-            scene.RemoveObject(this);
-        }
-    }
-    
-    internal class MyPlayer : Player
-    {
-        public MyPlayer(Vector2 position, Vector2 size, float directionAngle) : base(position, size, directionAngle)
-        {
-        }
-
-        public override void OnWorldUpdate(Scene scene, float elapsedMilliseconds)
-        {
-            if (Endurance < MaxEndurance)
-            {
-                Endurance += elapsedMilliseconds * 0.005f;
-                Endurance = MathF.Min(Endurance, MaxEndurance);
-            }
-            
-            Weapon.UpdateAnimation(elapsedMilliseconds);
-        }
-
-        public override void OnLeftMeleeAttack(Scene scene, MeleeWeapon weapon)
-        {
-        }
-
-        public override void OnRightMeleeAttack(Scene scene, MeleeWeapon weapon)
-        {
-         
-        }
-
-        public override void OnShoot(Scene scene, ShootingWeapon weapon)
-        {
-            
-        }
-    }
-
-    internal class GreenLight : BaseStaticMapObject
-    {
-        public GreenLight(Vector2 position, Vector2 size, float directionAngle, Sprite sprite) : base(position, size, directionAngle, sprite)
-        {
-        }
-
-        public override void OnWorldUpdate(Scene scene, float elapsedMilliseconds)
-        {
-            //nothing
-        }
-    }
-    internal class Sword : MeleeWeapon
-    {
-        public Sword(Animation staticAnimation, Animation attackLeftAnimation, Animation attackRightAnimation,
-            Animation blockLeftAnimation, Animation blockRightAnimation, Animation movingAnimation) : base(
-            staticAnimation, attackLeftAnimation, attackRightAnimation, blockLeftAnimation, blockRightAnimation,
-            movingAnimation)
-        {
-        }
-    }
-
-    internal class Arrow : IMapObject
-    {
-        public Arrow(Vector2 position, Sprite sprite, float directionAngle)
-        {
-            Position = position;
-            Sprite = sprite;
-            DirectionAngle = directionAngle;
-        }
-
-        public Vector2 Position { get; private set; }
-        public Vector2 Size { get; } = Vector2.One;
-        public float DirectionAngle { get; }
-        public Sprite Sprite { get; }
-
-        public void OnWorldUpdate(Scene scene, float elapsedMilliseconds)
-        {
-            var xRayUnit = MathF.Sin(DirectionAngle);
-            var yRayUnit = MathF.Cos(DirectionAngle);
-            var dx = xRayUnit * 0.05f * elapsedMilliseconds;
-            var dy = yRayUnit * 0.05f * elapsedMilliseconds;
-            var newPosition = Position + new Vector2(dx, dy);
-            var testX = (int) newPosition.X;
-            var testY = (int) newPosition.Y;
-
-            if (!scene.Map.InBound(testY, testX) || scene.Map.At(testY, testX).Type == MapCellType.Wall)
-            {
-                Console.WriteLine($"hit wall at {testY} {testX}");
-                scene.RemoveObject(this);
-                return;
-            }
-
-            var vertices = this.GetRotatedVertices();
-            foreach (var obj in scene.Objects)
-            {
-                if (obj == this)
-                    continue;
-
-                if (GeometryHelper.IsRectanglesIntersects(vertices, obj.GetRotatedVertices()))
-                {
-                    Console.WriteLine($"hit {obj} at {newPosition}");
-                    obj.OnShoot(scene, null);
-                    scene.RemoveObject(this);
-                    return;
-                }
-            }
-
-            Position = newPosition;
-        }
-
-        public void OnLeftMeleeAttack(Scene scene, MeleeWeapon weapon)
-        {
-        }
-
-        public void OnRightMeleeAttack(Scene scene, MeleeWeapon weapon)
-        {
-        }
-
-        public void OnShoot(Scene scene, ShootingWeapon weapon)
-        {
-        }
-    }
-
-    internal class Bow : ShootingWeapon
-    {
-        private readonly Sprite arrowSprite;
-        
-        public Bow(
-            Animation staticAnimation,
-            Animation movingAnimation,
-            Animation shootingAnimation,
-            Sprite arrowSprite) : base(staticAnimation, movingAnimation, shootingAnimation)
-        {
-            this.arrowSprite = arrowSprite;
-        }
-
-        public override void MakeShoot(Scene scene)
-        {
-            scene.AddObject(new Arrow(scene.Player.Position, arrowSprite, scene.Player.DirectionAngle));
-        }
-    }
-
     internal class Invisible : BaseStaticMapObject
     {
         public Invisible(Vector2 position, Vector2 size, float directionAngle) : base(position, size, directionAngle, new Sprite(new [] {1}, 1,1))
@@ -237,42 +27,31 @@ namespace menu
     {
         private static void Main(string[] args)
         {
-            using var engine = EngineBuilder.BuildEngine25D(new EngineOptions("simple 3d game", 720, 1280, false));
+            using var engine = EngineBuilder.BuildEngine25D(new EngineOptions("simple 3d game", 720, 1280, false, "./fonts/PressStart2P.ttf"));
             var player = new MyPlayer(new Vector2(2.0f, 7.0f), new Vector2(0.3f, 0.3f), MathF.PI);
+            var loader = new ResourceCachedLoader();
             var wallTexture = Sprite.Load("./sprites/greystone.png");
             var floorTexture = Sprite.Load("./sprites/colorstone.png");
             var ceilingTexture = Sprite.Load("./sprites/wood.png");
-            var greenLightTexture = Sprite.Load("./sprites/greenlight.png");
             var startButtonTexture = Sprite.Load("./sprites/startbutton_v4.png");
             var exitButton = Sprite.Load("./sprites/exitbutton_v1.png");
             var controlsText = Sprite.Load("./sprites/controls_v4.png");
             var scoreboard = Sprite.Load("./sprites/scoreboard.png");
             var statusBarInfo = Sprite.Load("./sprites/statusbarinfo.png");
             var tutorialEnd = Sprite.Load("./sprites/tutorialend.png");
-            var ghost = Animation.LoadFromDirectory("./animations/ghost");
-            
-            var sword = new Sword(
-                Animation.LoadFromDirectory("./animations/sword_static"),
-                Animation.LoadFromDirectory("./animations/sword_static"),
-                Animation.LoadFromDirectory("./animations/sword_static"),
-                Animation.LoadFromDirectory("./animations/sword_left_block"),
-                Animation.LoadFromDirectory("./animations/sword_right_block"),
-                Animation.LoadFromDirectory("./animations/sword_static"));
-            var bow = new Bow(
-                Animation.LoadFromDirectory("./animations/bow_static"),
-                Animation.LoadFromDirectory("./animations/bow_moving"),
-                Animation.LoadFromDirectory("./animations/bow_shoot"),
-                Sprite.Load("./sprites/arrow.png"));
+
+            var sword = Sword.Create(loader);
+            var bow = Bow.Create(loader);
             player.Weapons = new Weapon[] {bow, sword};
 
             var storage = new MapTextureStorage(ceilingTexture, wallTexture, floorTexture, controlsText, 
                 startButtonTexture, exitButton, scoreboard, statusBarInfo, tutorialEnd);
             var objects = new IMapObject[]
             {
-                new GreenLight(new Vector2(2.0f, 4.0f), new Vector2(0, 0), 0, greenLightTexture),
-                new GreenLight(new Vector2(6.0f, 4.0f), new Vector2(0, 0), 0, greenLightTexture),
+                GreenLight.Create(loader, new Vector2(2.0f, 4.0f), new Vector2(0, 0), 0),
+                GreenLight.Create(loader, new Vector2(6.0f, 4.0f), new Vector2(0, 0), 0),
                 new Invisible(new Vector2(9.0f, 3.0f), new Vector2(0.1f, 10.0f), 0), 
-                new Ghost(new Vector2(8.5f, 2.5f), new Vector2(1.0f, 1.0f), 0,  ghost) 
+                Ghost.Create(loader, new Vector2(8.5f, 2.5f), new Vector2(1.0f, 1.0f), 0),
             };
             var map = Map.FromStrings(new[]
             {
